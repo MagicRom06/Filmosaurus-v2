3f6d19fa855092898e98c386b70adac4a3962fc1
MagicRom06 <romainhenry60@gmail.com>
2021-11-06 13:22:23 +0100

    flake8 and isort

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,9 +1,11 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
 from .managers import CustomUserManager
 
 # Create your models here.
+
 
 class CustomUser(AbstractUser):
     username = None
