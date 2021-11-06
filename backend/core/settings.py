1c8ae533d01784c1af903bdf4ff1259e77bc60af
MagicRom06 <romainhenry60@gmail.com>
2021-11-06 12:57:46 +0100

    add custom user

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +134,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom users config
+AUTH_USER_MODEL = 'users.CustomUser'
