122786555ed4092ac08596cab5dff1528c05fb46
MagicRom06 <romainhenry60@gmail.com>
2021-11-07 08:44:42 +0100

    update pagination to get page param on url

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -146,6 +146,6 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # Rest frameworl config
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.LimitOffsetPagination',
+        'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
 }
