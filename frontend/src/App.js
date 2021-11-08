58b330935dd75f22c2aec694557ce37d1b8ebebb
MagicRom06 <romainhenry60@gmail.com>
2021-11-08 13:07:27 +0100

    ajax call and display first results

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -78,22 +78,105 @@ const moviesReducer = (state, action) => {
 
 const App = () => {
 
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const [url, setUrl] = React.useState(
+    `${API_ENDPOINT}${searchTerm}`
+  )
+
+  const [movies, dispatchMovies] = React.useReducer(
+    moviesReducer,
+    { data: [], isLoading : false, isError: false}
+  );
+  console.log(searchTerm)
+  console.log(url)
+  console.log(movies)
+  
+  const handleFetchMovies = React.useCallback(() => {
+    if (!searchTerm) return;
+    dispatchMovies({type: 'MOVIES_FETCH_INIT'})
+    axios
+      .get(url)
+      .then(result => {
+        dispatchMovies({
+          type: 'MOVIES_FETCH_SUCCESS',
+          payload: result.data
+        })
+      })
+      .catch(() => 
+        dispatchMovies({type: 'MOVIES_FETCH_FAILURE'})
+      );
+  }, [url])
+
+  React.useEffect(() => {
+    handleFetchMovies()
+  }, [handleFetchMovies])
+
+  const handleSearchInput = event => {
+    setSearchTerm(event.target.value);
+  }
+
+  const handleSearchSubmit = event => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`)
+    event.preventDefault();
+  }
+
   return (
     <>
     <Header />
-    <SearchForm />
+    <SearchForm
+      searchTerm={searchTerm}
+      onSearchInput={handleSearchInput}
+      onSearchSubmit={handleSearchSubmit}
+    />
+    {movies.isError && <p>Something went wrong ...</p>}
+    {movies.isLoading ? ( <p>Loading ...</p> ) : (
+       movies.data.results !== undefined && <List list={movies.data} />
+    )}
     </>
   );
 }
 
-const SearchForm = () => {
+const SearchForm = ({searchTerm, onSearchInput, onSearchSubmit}) => {
   return (
-      <StyledForm>
-          <StyledLabel>Search : </StyledLabel>
-          <StyledInput />
-          <StyledButton>Search</StyledButton>
+      <StyledForm onSubmit={onSearchSubmit}>
+          <InputWithLabel
+            id="search"
+            value={searchTerm}
+            type="text"
+            onInputChange={onSearchInput}
+          >
+            Search : 
+          </InputWithLabel>
+          <StyledButton type="submit" disabled={!searchTerm}>Search</StyledButton>
       </StyledForm>
   )
 }
+
+const InputWithLabel = ({id, value, type, onInputChange, children}) => {
+
+  const inputRef = React.useRef();
+
+  return (
+    <>
+    <StyledLabel htmlFor={id}>{children}</StyledLabel>
+    <StyledInput 
+      ref={inputRef}
+      onChange={onInputChange}
+      id={id}
+      type={type}
+      value={value}
+    />
+    </>
+  )
+}
+
+const List = React.memo(({list}) => {
+  console.log(list.results)
+  return list.results.map(item => {
+    return (
+        <p>{item.title}</p>
+    )
+  })
+})
 
 export default App;
