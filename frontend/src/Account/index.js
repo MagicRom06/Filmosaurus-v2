09b71a6806858e9f3fdf3fee8c7b77a1d9d7feb9
MagicRom06 <romainhenry60@gmail.com>
2021-12-09 14:21:36 +0100

    account page first render

--- frontend/src/Account/index.js
+++ frontend/src/Account/index.js
@@ -1,5 +1,41 @@
 import axios from 'axios';
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div `
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+`;
+
+const StyledHeadlinePrimary = styled.h1 `
+    font-size: 35px;
+    font-weight: 300;
+    letter-spacing: 2px;
+`;
+
+const StyledHeadlineSecondary = styled.h1 `
+    font-size: 30px;
+    font-weight: 300;
+    letter-spacing: 2px;
+    border-bottom: 1px solid #171212;
+    padding: 15px;
+    margin: 0;
+`;
+
+const StyledRowDiv = styled.div `
+    display: flex;
+    justify-content: center;
+`;
+
+const StyledBlockDiv = styled.div `
+    border: 1px solid #171212;
+    width: 30%;
+    padding: 5px;
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+`;
 
 const Account = ({token}) => {
     const [watchList, setWatchList] = React.useState([])
@@ -21,10 +57,24 @@ const Account = ({token}) => {
         handleLoadWatchlist()
     }, [handleLoadWatchlist])
 
-    console.log(watchList)
-
     return (
-        <h3>Account</h3>
+        <>
+        <StyledContainer>
+        <StyledHeadlinePrimary>Account</StyledHeadlinePrimary>
+            <StyledRowDiv>
+                <StyledBlockDiv>
+                    <StyledHeadlineSecondary>Watchlist</StyledHeadlineSecondary>
+                    <div>2</div>
+                </StyledBlockDiv>
+                <StyledBlockDiv>
+                    <StyledHeadlineSecondary>Historic</StyledHeadlineSecondary>
+                </StyledBlockDiv>
+                <StyledBlockDiv>
+                    <StyledHeadlineSecondary>Personal</StyledHeadlineSecondary>
+                </StyledBlockDiv>
+            </StyledRowDiv>
+        </StyledContainer>
+        </>
     )
 }
 
